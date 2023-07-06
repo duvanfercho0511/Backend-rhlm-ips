63883cbcd53cd8ba0275d3f542e05acb26f3dbed
Duvan Vargas <duvan2191120@correo.uis.edu.co>
2023-07-06 13:59:10 -0500

    First commit

---
+++ src/main/java/com/rhlmips/rhlmips/RhlmIpsApplication.java
@@ -0,0 +1,13 @@
+package com.rhlmips.rhlmips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhlmIpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhlmIpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhlmips/rhlmips/RhlmIpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhlmips.rhlmips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhlmIpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
